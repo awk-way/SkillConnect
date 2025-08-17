0f2651d4f461049d5d10c50f34f3b0883ea0b3bd
Rajashree Jarande <awk.way3011@gmail.com>
2025-08-17 15:56:47 +0530

    Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'pages/login.dart';
 import 'pages/signup.dart';
 import 'pages/home.dart';
 import 'pages/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(SkillConnectApp());
 }
 
