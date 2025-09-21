9e04bed43c6405f1dc3cd671ba4c6d22e35a6b63
Rajashree Jarande <awk.way3011@gmail.com>
2025-09-22 03:13:36 +0530

    dk why

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,12 @@
 buildscript {
-  dependencies {
-    classpath 'com.google.gms:google-services:4.4.2'
-  }
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
 }
 
 allprojects {
